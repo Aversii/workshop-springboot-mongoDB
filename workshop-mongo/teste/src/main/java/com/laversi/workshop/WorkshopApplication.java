06e261b01fe7eb45245ff8e2077be584b524fc02
aversii <lucasaversi@gmail.com>
2023-06-24 16:10:05 -0300

    folder ajustment

--- workshop-mongo/teste/src/main/java/com/laversi/workshop/WorkshopApplication.java
+++ workshop-mongo/teste/src/main/java/com/laversi/workshop/WorkshopApplication.java
@@ -1,13 +1,13 @@
-package com.laversi.teste;
+package com.laversi.workshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TesteApplication {
+public class WorkshopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TesteApplication.class, args);
+		SpringApplication.run(WorkshopApplication.class, args);
 	}
 
 	
