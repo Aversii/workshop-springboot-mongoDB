75c91d10df9df94abadc5975fa30634fba074d65
aversii <lucasaversi@gmail.com>
2023-06-25 01:48:15 -0300

    Creating entity post nesting entity user

--- workshop-mongo/teste/src/main/java/com/laversi/workshop/config/Instantiation.java
+++ workshop-mongo/teste/src/main/java/com/laversi/workshop/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.laversi.workshop.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.laversi.workshop.domain.Post;
 import com.laversi.workshop.domain.User;
+import com.laversi.workshop.repository.PostRepository;
 import com.laversi.workshop.repository.UserRepository;
 
 @Configuration
@@ -14,15 +18,31 @@ public class Instantiation implements CommandLineRunner {
 
     @Autowired
     private UserRepository userRepo;
+    @Autowired
+    private PostRepository postRepo;
     
 
     @Override
     public void run(String... args) throws Exception {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         userRepo.deleteAll();
-        User pedro = new User(null, "Pedro", "pedro@gmail.com");
-        User manoela = new User(null, "Manoela", "manoela@gmail.com");
-        User yuri = new User(null, "Yuri", "yuri@gmail.com");
-        userRepo.saveAll(Arrays.asList(pedro, manoela, yuri));        
+        postRepo.deleteAll();
+
+        User pedro = new User("1", "Pedro", "pedro@gmail.com");
+        User manoela = new User("2", "Manoela", "manoela@gmail.com");
+        User yuri = new User("3", "Yuri", "yuri@gmail.com");
+
+        Post post1 = new Post(null, sdf.parse("23/01/2018"), "Partiu Viagem", "Fui para Tangamandápio",pedro);
+        Post post2 = new Post(null, sdf.parse("23/05/2018"), "Partiu Trabalho", "Fui para o Trabalho =/",pedro);
+        Post post3 = new Post(null, sdf.parse("25/12/2018"), "Partiu Natal", "Fui para o polo norte",yuri);
+
+               
+        userRepo.saveAll(Arrays.asList(pedro, manoela, yuri));
+        postRepo.saveAll(Arrays.asList(post1,post2,post3));     
+        
     }
     
     
