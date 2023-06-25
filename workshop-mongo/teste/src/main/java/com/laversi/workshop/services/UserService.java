df5ce5ab8bdf137c4c3cd506113d0ed1bf641d87
aversii <lucasaversi@gmail.com>
2023-06-24 23:36:57 -0300

    now is possible insert a new user using endpoint POST

--- workshop-mongo/teste/src/main/java/com/laversi/workshop/services/UserService.java
+++ workshop-mongo/teste/src/main/java/com/laversi/workshop/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.laversi.workshop.domain.User;
+import com.laversi.workshop.dto.UserDTO;
 import com.laversi.workshop.repository.UserRepository;
 import com.laversi.workshop.services.exception.ObjectNotFoundException;
 
@@ -27,6 +28,14 @@ public class UserService {
 
         }
         return user.get();
+    }
+
+    public User insert(User obj){
+        return userRepo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());        
     }
 
 
