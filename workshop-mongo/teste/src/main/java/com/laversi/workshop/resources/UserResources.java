e6f0c9a4e5e9a38b22ab2dd46b57f86dd171a3b1
aversii <lucasaversi@gmail.com>
2023-06-25 02:39:52 -0300

    Endpoint to return posts from author id

--- workshop-mongo/teste/src/main/java/com/laversi/workshop/resources/UserResources.java
+++ workshop-mongo/teste/src/main/java/com/laversi/workshop/resources/UserResources.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.laversi.workshop.domain.Post;
 import com.laversi.workshop.domain.User;
 import com.laversi.workshop.dto.UserDTO;
 import com.laversi.workshop.services.UserService;
@@ -70,7 +71,15 @@ public class UserResources {
     public ResponseEntity<Void> delete(@PathVariable String id){
         service.delete(id);
         return ResponseEntity.noContent().build();
-    } 
+    }
+    
+    //author posts
+
+     @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
+    }  
     
     
 }
